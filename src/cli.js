7d8fe6f42a76c3b4c87720dbdaf73f13faec8235
Matvey Karabanov <RaZRaBoT4IKtop@gmail.com>
2024-11-26 12:17:17 +0700

    Created src/cli.js and added function, created package-lock.json

---
+++ src/cli.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+export const helloUserName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+
+  console.log(`Hello, ${userName}!`);
+
+  return userName;
+}
+
